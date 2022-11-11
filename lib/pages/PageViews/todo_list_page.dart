3d225e14112e186bdab29a5f82eafa57972f7397
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2022-11-11 14:13:56 -0300

    atualizando projeto

--- lib/pages/PageViews/todo_list_page.dart
+++ lib/pages/PageViews/todo_list_page.dart
@@ -33,27 +33,51 @@ class _TodoListPageState extends State<TodoListPage> {
             ),
             Container(
               height: 400,
-              child: ListView.builder(
+              child: ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
                 itemCount: tarefas.length,
                 itemBuilder: (context, index) {
-                  return Text(tarefas[index]);
+                  return ListTile(
+                    title: Text(tarefas[index]),
+                    onLongPress: () {
+                      setState(() {
+                        tarefas.removeAt(index);
+                      });
+                    },
+                  );
                 },
               ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.green,
-        onPressed: () {
-          if (_textEditingController.text.length > 0) {
-            setState(() {
-              tarefas.add(_textEditingController.text);
-            });
-            _textEditingController.clear();
-          }
-        },
-        child: Icon(Icons.add),
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              if (_textEditingController.text.length > 0) {
+                setState(() {
+                  tarefas.add(_textEditingController.text);
+                });
+                _textEditingController.clear();
+              }
+            },
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              setState(() {
+                tarefas = [];
+              });
+              _textEditingController.clear();
+            },
+            child: Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
